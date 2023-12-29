df63e2d564bda962d9e78bb0f0a93c988fbda48b
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-12-29 15:17:35 +0300

    vue refactoring

--- vue/frontend/src/main.js
+++ vue/frontend/src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import bootstrapCss from "./dist/bootstrap_5-3/css/bootstrap.min.css";
+import JqueryJs from "./dist/jquery/jquery_3.7.1.js";
+import bootstrapJs from "./dist/bootstrap_5-3/js/bootstrap.bundle.min.js";
 
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
