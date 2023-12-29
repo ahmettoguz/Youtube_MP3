4d445dcf0ba8caa55dda319ce68312baee336449
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-12-29 22:31:06 +0300

    refactor

--- Backend/src/endpoint/health-check/backendHealthCheck.js
+++
@@ -1,7 +0,0 @@
-const expressService = require("../../service/expressService");
-
-const backendHealthCheck = async (req, res) => {
-  return expressService.returnResponse(res, 200, "Service is up.");
-};
-
-module.exports = backendHealthCheck;
