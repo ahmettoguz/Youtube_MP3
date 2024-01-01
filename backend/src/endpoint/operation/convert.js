4b1e60e4d1efb05ab8e43ede7f93d240480a340a
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-01-02 02:18:44 +0300

    Img download is added.

--- backend/src/endpoint/operation/convert.js
+++ backend/src/endpoint/operation/convert.js
@@ -2,16 +2,35 @@ const expressService = require("../../service/expressService");
 const commonService = require("../../service/commonService");
 const ytService = require("../../service/ytService");
 const fileService = require("../../service/fileService");
+const httpService = require("../../service/httpService");
 const path = require("path");
 
 const getUrlInfo = async (req, res) => {
   // get user id to use folder
   const userId = commonService.getHeaderValue(req, "user-id");
 
+  // get image url to download cover image
+  const coverImgUrl = req.body.videoBanner;
+
   // Check if the user's folder exists, and create it if not
   const songId = commonService.generateRandomWord();
   const userFolderPath = path.join(__dirname, "../../storage", userId, songId);
   fileService.createFolderIfNotExist(userFolderPath);
+
+  // download cover image
+  const imgResponse = await httpService.downloadImage(
+    coverImgUrl,
+    path.join(userFolderPath, "./cover.webp")
+  );
+
+  // check cover image
+  if (!imgResponse.status) {
+    return expressService.returnResponse(
+      res,
+      500,
+      "Cover page cannot donwloaded."
+    );
+  }
 
   // start download
   const status = await ytService.downloadToServer(path.join(userFolderPath));
