0b45dc2adaaf6df058b508ff3fa343582c494d15
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-01-03 12:29:30 +0300

    restart checked.

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,6 @@ const app = runApp();
 
 dotenv.config();
 
-setTimeout(() => {
-  console.log("exitingg");
-  process.exit(0); 
-}, 5000);
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.info(`App is running on : http://localhost:${PORT}`)
