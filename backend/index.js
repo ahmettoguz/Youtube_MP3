8612e8453fae83d952016bc02d634cb542e22f2c
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-01-03 12:08:25 +0300

    docker compose updated.

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,11 @@ const app = runApp();
 
 dotenv.config();
 
+setTimeout(() => {
+  console.log("exitingg");
+  process.exit(0); 
+}, 5000);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.info(`App is running on : http://localhost:${PORT}`)
