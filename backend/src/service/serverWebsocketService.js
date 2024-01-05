f973e213ab2e12ed36c8cee0dc8190554f78bfd9
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-01-05 19:31:27 +0300

    wss in progress.

--- backend/src/service/serverWebsocketService.js
+++ backend/src/service/serverWebsocketService.js
@@ -11,10 +11,7 @@ class ServerWebsocketService {
     this.currentClientId = null;
     this.currentWs = null;
 
-    this.wsServer = new WebSocket.Server({
-      server: sslService.getWssServer(),
-      port: port,
-    });
+    this.wsServer = new WebSocket.Server({ server: sslService.getWssServer() });
     // this.wsServer = new WebSocket.Server({ port: port });
     this.wsServer.on("connection", (ws) => {
       this.currentWs = ws;
